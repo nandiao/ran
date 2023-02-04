3b2502e92aa460d943c3d7f10c88baf5f52f642f
karl <nandiao@qq.com>
2023-02-04 15:39:00 +0800

    some

--- server/public/index.php
+++ server/public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * COPYRIGHT (C), Yun Shang. Co., Ltd.
  * Author:  lookan
@@ -9,13 +8,18 @@
 // [ 应用入口文件 ]
 namespace ran;
 
-define("PUBLIC_DIR", __DIR__);
+date_default_timezone_set('PRC');
+/*
+ * 常量设置
+ */
+define("DS", DIRECTORY_SEPARATOR); //目录分隔符简化
+define("PUBLIC_DIR", __DIR__); //public目录地址
 
 // 加载基础文件
-require __DIR__ . '/../../../ran/ran.php';
+require __DIR__ . '/../ran/Ran.php';
 
 // 加载函数库
-require __DIR__ . '/../../../ran/common.php';
+require __DIR__ . '/../ran/common.php';
 
 //执行然框架单例
 Ran::getInstance();
